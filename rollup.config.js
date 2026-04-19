367a1d0f689003e680fe52878441dc4f46ea7b80
z3dev <z3-dev@gfnews.net>
2026-04-19 14:21:07 +0900

    build(jscad-object-api): initial V3 build

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import * as fs from 'fs'
+
+import terser from '@rollup/plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+const { name, version, license } = JSON.parse(fs.readFileSync('package.json'))
+
+export default {
+  input: 'src/index.js',
+  external: ['@jscad/modeling'],
+  output: [
+    {
+      file: 'dist/jscad-object-api.min.js',
+      format: 'umd',
+      name: 'jscad-object-api',
+      banner: `/*! ${name} V${version} (${license}) */`,
+      globals: { "@jscad/modeling": "jscadModeling"}
+    },
+    {
+      file: 'dist/jscad-object-api.es.js',
+      format: 'es',
+      banner: `/*! ${name} V${version} (${license}) */`
+    }
+  ],
+  plugins: [
+    nodeResolve(),
+    terser({ compress: { module: true }, mangle: false, format: { comments: 'some' } })
+  ]
+}
